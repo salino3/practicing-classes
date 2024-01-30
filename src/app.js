4ebb416ff1ebc9e3dd5e4a2b4cbc38d2259f45c0
salino3 <programacionflavio@gmail.com>
2024-01-30 17:09:17 +0100

    commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 console.log("Running front app");
 
+//
+  let circle = document.getElementById("firstCircle");
+
+  let circleWidth = circle.offsetWidth;
+  console.log(circleWidth);
+
+circle.style.height =  circleWidth + "px";
